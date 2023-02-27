5968bcdef663e00ba6e8654ecf0a520403782d85
Pavel <95717191+PabloKarpacho@users.noreply.github.com>
2023-02-27 21:01:24 +0300

    Update nodes.py

--- bamt/nodes.py
+++ bamt/nodes.py
@@ -70,8 +70,8 @@ class BaseNode(object):
             if type(model).__name__ == "CatBoostRegressor":
                 a = model_ser.encode('latin1')
                 
-            if type(model).__name__ == "Fedot":
-                a = model_ser.encode('latin1')
+#             if type(model).__name__ == "Fedot":
+#                 a = model_ser.encode('latin1')
                 
             else:
                 a = model_ser.replace('\"', '\'').encode('latin1')
